0cf471654658352a9e36bcf643c9668369696796
VisaliSR <visalirajkumar57@gmail.com>
2026-08-04 21:57:52 +0530

    Add model persistence and inference workflow

---
+++ src/predict.py
@@ -0,0 +1,32 @@
+import joblib
+import pandas as pd
+
+pipeline=joblib.load("models/balanced_logistic_regression.pkl")
+
+new_customer = pd.DataFrame([
+    {
+        "gender": "Female",
+        "SeniorCitizen": 0,
+        "Partner": "Yes",
+        "Dependents": "No",
+        "tenure": 2,
+        "PhoneService": "Yes",
+        "MultipleLines": "No",
+        "InternetService": "Fiber optic",
+        "OnlineSecurity": "No",
+        "OnlineBackup": "No",
+        "DeviceProtection": "No",
+        "TechSupport": "No",
+        "StreamingTV": "Yes",
+        "StreamingMovies": "Yes",
+        "Contract": "Month-to-month",
+        "PaperlessBilling": "Yes",
+        "PaymentMethod": "Electronic check",
+        "MonthlyCharges": 95.0,
+        "TotalCharges": 190.0
+    }
+])
+prediction=pipeline.predict(new_customer)
+print(f"Customer churn possibility {prediction}")
+prob=pipeline.predict_proba(new_customer)
+print(f"Customer churn probability {prob}")
